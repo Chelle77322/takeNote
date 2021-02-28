257f367ba2dbc96ece4a51b75bc8d0442c874ce1
Chelle77322 <serenynadarra@gmail.com>
2021-02-28 20:07:24 +1030

    Still trying not to get this killed

--- start.js
+++ start.js
@@ -2,7 +2,8 @@
 const express = require("express");
 //const path = require("path");
 //const fs = require("fs");
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
+console.log(PORT);
 
 //Making sure the db.json file is doing what it should
 const dbJson = require('./db/db.json')
