e3958a038b7973df1e912c11df079fa9d66e9ffa
Chelle77322 <serenynadarra@gmail.com>
2021-02-28 21:18:37 +1030

    Making changes to the directory

--- start.js
+++ start.js
@@ -15,15 +15,15 @@ var app = express();
 //Accesses the public files so it looks all pretty like
 
 app.use(express.static(__dirname +'./public'));
-app.use(express.static('./'));
+//app.use(express.static('./'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 //api and html routing files for communication purposes
-require("./apiRouting")(app);
-require("./htmlRouting")(app);
+require('./routes/apiRouting')(app);
+require('./routes/htmlRouting')(app);
 
 //Start the port listening
-app.listen(port, LOCAL_ADDRESS ='127.0.0.0', function (){
+app.listen(port, LOCAL_ADDRESS ='127.0.0.1',()=>{
     console.log("App listening on port"+ port);
 });
